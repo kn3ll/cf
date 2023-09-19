933a6601224235ed4adbdcab911e67eab83402f6
David Šparlinek <david.sparlinek@mavenir.com>
2023-09-19 20:33:23 +0200

    update for zig 0.11

--- build.zig
+++ build.zig
@@ -3,14 +3,12 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const mode = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("cf", "cf.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const lib = b.addStaticLibrary(.{ .name = "cf", .root_source_file = .{ .path = "cf.zig"}, .target = .{}, .optimize = mode});
+    b.installArtifact(lib);
 
-    const main_tests = b.addTest("cf.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{ .root_source_file = .{ .path = "cf.zig"}, .optimize = mode});
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
